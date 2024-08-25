142aa8e39753894e6cf7ba4476e77b4dbb666e9a
mswrks <moskovnm@gmail.com>
2024-08-25 19:14:55 +0300

    firstomne

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <>
+    <div className="home">
+      <li className='home-home-but'>Home</li>
+      <li className='home-sale-but'>Sale</li>
+      <li className='home-market-but'>Market</li>
+    </div>
+    </>
+  );
+}
+
+export default Home;
